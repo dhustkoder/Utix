ec8b36659b2f3f1b523afdaafa4f49257783e6d8
dhust <rafaelmoura.dev@gmail.com>
2016-07-04 00:18:37 -0300

    NotNull improved

--- Utix/include/Utix/NotNull.h
+++ Utix/include/Utix/NotNull.h
@@ -1,5 +1,6 @@
 #ifndef UTIX_NOTNULL_H_
 #define UTIX_NOTNULL_H_
+#include "Ints.h"
 #include "Assert.h"
 #include "BaseTraits.h"
 
@@ -13,33 +14,43 @@ class NotNull
   static_assert(is_pointer<T>::value, "T must be a pointer type. Ex: NotNull<int*> not NotNull<int>");
 
 public:
-	NotNull(const NotNull&) = delete;
 	NotNull(NotNull&&) = delete;
-	NotNull& operator=(const NotNull&) = delete;
+	NotNull(nullptr_t) = delete;
+	NotNull(int) = delete;
 	NotNull& operator=(NotNull&&) = delete;
+	NotNull& operator=(nullptr_t) = delete;
+	NotNull& operator=(int) = delete;
+
 	NotNull(T p);
-	decltype(*T()) operator*();
-	T operator->();
-	T Get();
+	NotNull(const NotNull&) = default;
+	NotNull& operator=(const NotNull&) = default;
+	operator T() const;
+	decltype(*T()) operator*() const;
+	T operator->() const;
+	T Get() const;
 
 private:
-	T _p;
+	T m_p;
 };
 
 
 template<class T>
-inline NotNull<T>::NotNull(T p) 
-	: _p(p)
+inline NotNull<T>::NotNull(T not_null_ptr) 
+	: m_p(not_null_ptr)
 {
-	ASSERT_MSG(_p != nullptr, "NotNull must not hold null pointers");
+	ASSERT_MSG(not_null_ptr != nullptr, "NotNull must not hold null pointers");
 }
 
+
+
 template<class T>
-inline decltype(*T()) NotNull<T>::operator*() { return *_p; }
+inline NotNull<T>::operator T() const { return Get(); }
 template<class T>
-inline T NotNull<T>::operator->() { return _p; }
+inline decltype(*T()) NotNull<T>::operator*() const { return *Get(); }
 template<class T>
-inline T NotNull<T>::Get() { return _p; }
+inline T NotNull<T>::operator->() const { return Get(); }
+template<class T>
+inline T NotNull<T>::Get() const { return m_p; }
 
 
 
--- Utix/src/Test/test.cpp
+++ Utix/src/Test/test.cpp
@@ -1,20 +1,31 @@
+#include <cstdio>
 #include <iostream>
+#include <memory>
 #include <Utix/NotNull.h>
+
+
 using namespace utix;
 
 class Bug {
 public:
-	Bug() {}
-	~Bug() {}
+	Bug() { std::cout << "A bug is born!\n"; }
+	~Bug() { std::cout << "A bug has died!\n";}
 	void CallBug() { std::cout << "You're the shit!\n"; }
 };
 
 
 
-
-int main(int argc, char** argv)
+#define LOG(x, y) std::printf("%s: %zX\n",x, reinterpret_cast<size_t>(y));
+int main()
 {
-	NotNull<Bug*> bug(new(std::nothrow) Bug());
-
-	(*bug).CallBug();
+	NotNull<Bug*> b1( new Bug() );
+	LOG("b1", b1.Get());
+	auto b2 = b1;
+	LOG("b2", b2.Get());
+	LOG("b1", b1.Get());
+	b1 = b2;
+	LOG("b2", b2.Get());
+	LOG("b1", b1.Get());
+	delete b1;
+	
 }
