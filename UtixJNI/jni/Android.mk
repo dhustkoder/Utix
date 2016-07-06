74c35f357255f1002b1db9c30ecbdfb536145f82
dhust <rafaelmoura.dev@gmail.com>
2016-07-06 08:37:54 -0300

    Android mk Update

--- UtixJNI/jni/Android.mk
+++ UtixJNI/jni/Android.mk
@@ -2,13 +2,15 @@ UTIX_JNI_PATH := $(call my-dir)
 
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := Utix
 
 UTIX_SRC_DIR :=${UTIX_JNI_PATH}/../../Utix/src/Utix/
 UTIX_INCLUDE_DIR :=${UTIX_JNI_PATH}/../../Utix/include/
 
 
-LOCAL_MODULE := Utix
-LOCAL_C_INCLUDES := ${UTIX_INCLUDE_DIR} ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include
+
+
+LOCAL_C_INCLUDES := ${UTIX_INCLUDE_DIR} ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include/
 LOCAL_SRC_FILES := $(wildcard $(UTIX_SRC_DIR)*.cpp)
 
 LOCAL_CPPFLAGS := -std=c++11 -Wall -Wextra -O3 -fPIC -static
--- UtixJNI/jni/Application.mk
+++ UtixJNI/jni/Application.mk
@@ -1,3 +1,2 @@
-APP_BUILD_SCRIPT := Android.mk
 APP_STL := gnustl_static
 APP_ABI := armeabi armeabi-v7a x86
