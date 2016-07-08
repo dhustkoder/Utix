74152b272614a752b8072183f0cf275080326fa4
dhust <rafaelmoura.dev@gmail.com>
2016-07-08 18:12:48 -0300

    release / debug android makefile

--- UtixJNI/jni/Android.mk
+++ UtixJNI/jni/Android.mk
@@ -3,14 +3,19 @@ UTIX_JNI_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := Utix
-
 UTIX_SRC_DIR :=${UTIX_JNI_PATH}/../../Utix/src/Utix
 UTIX_INCLUDE_DIR :=${UTIX_JNI_PATH}/../../Utix/include
 
+
+LOCAL_CPPFLAGS := -std=c++11 -Wall -Wextra -fPIC -static -fno-exceptions
 LOCAL_C_INCLUDES := ${UTIX_INCLUDE_DIR} ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/4.9/include
 LOCAL_SRC_FILES := $(wildcard $(UTIX_SRC_DIR)/*.cpp)
 
-LOCAL_CPPFLAGS := -std=c++11 -Wall -Wextra -O3 -fPIC -static
+ifdef DEBUG
+LOCAL_CPPFLAGS += -D_DEBUG -O0 -g 
+else
+LOCAL_CPPFLAGS += -DNDEBUG -O3 -fomit-frame-pointer 
+endif
 LOCAL_STATIC_LIBRARIES := Utix log
 
 
